c4b9a1f165e19c549ad0191a2ad124a6fbb29b3c
Beryl Atieno <berylatieno30@gmail.com>
2024-09-20 21:48:32 +0300

    add: getHumanChoice()

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
     let randomIndex = Math.floor(Math.random() * choices.length)
     let computerChoice = choices[randomIndex]
     return computerChoice
+}
+
+function getHumanChoice() {
+
+    // TO-DO: write script to handle invalid input
+    let humanChoice = prompt("Please enter your choice: ");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice
 }
