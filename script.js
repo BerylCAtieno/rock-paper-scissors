b53254c1ee4104da8209dfa18cbd307fa4a00592
Beryl Atieno <berylatieno30@gmail.com>
2024-09-21 14:37:52 +0300

    add:play round logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"]
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choices.length)
@@ -12,4 +14,23 @@ function getHumanChoice() {
     let humanChoice = prompt("Please enter your choice: ");
     humanChoice = humanChoice.toLowerCase();
     return humanChoice
+}
+
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection == computerSelection) {
+        console.log(`it is a draw! You picked ${humanSelection} and computer picked ${computerSelection}`)
+        return "draw"
+    }
+    else if  ((humanSelection == "paper" && computerSelection == "rock") || 
+    (humanSelection == "rock" && computerSelection == "scissors") ||
+    (humanSelection == "scissors" && computerSelection == "paper")) {
+        humanScore++
+        console.log(`You win! ${humanSelection} beats ${computerSelection}`)
+        return "win"
+    } else {
+        computerScore++
+        console.log(`You lose! ${computerSelection} beats ${humanSelection}`)
+        return "loss"
+        }
+
 }
